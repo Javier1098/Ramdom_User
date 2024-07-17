e67231c1f540429fc04f483fcb2e8f6cc9ba1471
Javier Uribe <javieruribt@gamail.com>
2024-07-17 16:44:13 -0500

    Finish

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { RandomUserFetch } from './components/RandomUserFetch';
+
+
+function App() {
+
+  return (
+    <div className='text-white'>
+      <RandomUserFetch />
+    </div>
+  )
+}
+
+export default App
